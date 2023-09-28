b7a7ae6817821b26dde8348cf915b1b4c4a59395
AKSHAYAKS-03 <121333474+AKSHAYAKS-03@users.noreply.github.com>
2023-09-28 14:08:30 +0530

    Update Com.java

--- Com.java
+++ Com.java
@@ -1,34 +1,29 @@
+
 package com;
-import java.util.*;
-import loan.LoanAccount;
-
+import com.loan.LoanAccount;
 public class Com {
-
-    
     public static void main(String[] args) {
-        Scanner S = new Scanner(System.in);
-        System.out.println("enter nos");       
+        int n = args.length;
         int s = Integer.parseInt(args[0]);
         int c = Integer.parseInt(args[1]);
         int d = Integer.parseInt(args[2]);
         
-        
       LoanAccount la = new LoanAccount();
         la.Transaction(s,c,d);
-}    
+
+   }
 }
 
 
 // Transactio.java
 
-package transact;
+package com.transact;
 public class Transaction {
    int salary;
 
     public Transaction(int s){
         this.salary = s;
     }
-
     public int credits(int c){
         int res= salary+c;
        return res;
@@ -36,15 +31,12 @@ public class Transaction {
     public int debits(int d){
         int res1 =  salary-d;
         return res1;
- }
+    }
 }
-
-
 //LoanAccount.java
 
-package loan;
-import transact.*;
-
+package com.loan;
+import com.transact.*;
 public class LoanAccount {
     
     public void Transaction(int s,int c,int d) {
@@ -53,5 +45,6 @@ public class LoanAccount {
 
         System.out.println("the amount after credit:"+ta.credits(c));
         System.out.println("the amount after debit:"+ta.debits(d));
+        
     }
 }
